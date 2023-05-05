3a2f35516c1fbd45fa5781fa8c28642737bd1e3b
Patri <uo282210@uniovi.es>
2023-05-05 19:52:09 +0200

    logs api

--- sdi2223-entrega2-31/app.js
+++ sdi2223-entrega2-31/app.js
@@ -78,7 +78,7 @@ messagesRepository.init(app, MongoClient);
 require("./routes/users.js")(app, usersRepository, offersRepository, logRepository, log4js.getLogger("users"));
 require("./routes/admin.js")(app, usersRepository, logRepository, log4js.getLogger("admin"));
 require("./routes/offers.js")(app, usersRepository, offersRepository, logRepository, log4js.getLogger("offers"));
-require("./routes/api/APIv1.0.js")(app, usersRepository, offersRepository, conversRepository, messagesRepository);
+require("./routes/api/APIv1.0.js")(app, usersRepository, offersRepository, conversRepository, messagesRepository, logRepository, log4js.getLogger("api"));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
