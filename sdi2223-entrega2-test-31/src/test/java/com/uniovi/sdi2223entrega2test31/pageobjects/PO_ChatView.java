7f9a890965aa477fc08ac02eb500908039693873
enrique <uo282867@uniovi.es>
2023-05-07 18:13:29 +0200

    uo282867_Correccion error test y fin error ver conversaciones desde ofertas.

--- sdi2223-entrega2-test-31/src/test/java/com/uniovi/sdi2223entrega2test31/pageobjects/PO_ChatView.java
+++ sdi2223-entrega2-test-31/src/test/java/com/uniovi/sdi2223entrega2test31/pageobjects/PO_ChatView.java
@@ -1,11 +1,13 @@
 package com.uniovi.sdi2223entrega2test31.pageobjects;
 
+import com.uniovi.sdi2223entrega2test31.util.SeleniumUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class PO_ChatView extends PO_NavView {
     static public void createMessage(WebDriver driver, String message) {
+        SeleniumUtils.waitLoadElementsBy(driver, "id","newMessage", getTimeout());
         WebElement email = driver.findElement(By.id("newMessage"));
         email.click();
         email.clear();
